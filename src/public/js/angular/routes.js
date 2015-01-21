48729ac62f2f1fe53ff4f463362642d9a14d01cb
Feroj Bepari <feroj21@gmail.com>
2015-01-21 14:07:02 +0600

    Code structured

--- src/public/js/angular/routes.js
+++ src/public/js/angular/routes.js
@@ -1,18 +1,29 @@
 'user strict';
-app.config(['$routeProvider', function ($routeProvider) {
-        $routeProvider
-                .when('/profile', {
-                    templateUrl: 'template/profile.html',
-                    controller: 'ProfileCtrl'
-                })
-                .when('/user', {
-                    title: "User Page",
-                    templateUrl: 'template/user.html',
-                    controller: 'authCtrl'
-                })
-                 .when('/user/confirmation', {
-                    title: "Successfully Registered",
-                    templateUrl: 'template/user_success.html',
-                    controller: 'authCtrl'
-                });
-    }]);
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+            .when('/profile', {
+              templateUrl: 'template/profile.html',
+              controller: 'ProfileCtrl'
+            })
+            .when('/user', {
+              title: "User Page",
+              templateUrl: 'template/user.html',
+              controller: 'authCtrl'
+            })
+            .when('/user/confirmation', {
+              title: "Successfully Registered",
+              templateUrl: 'template/user_success.html',
+              controller: 'authCtrl'
+            })
+            .when('/user/edit', {
+              title: "Profile Edit",
+              templateUrl: 'template/profile_edit.html',
+              controller: 'ProfileCtrl'
+            })
+            .when('/user/verify', {
+              title: "Profile Edit",
+              templateUrl: 'template/profile_edit.html',
+              controller: 'ProfileCtrl'
+            });
+  }]);
+
