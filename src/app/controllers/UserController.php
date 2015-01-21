41525917c501cf14e602cd331cbc2e8f89d033e5
imrancluster <imrancluster@gmail.com>
2015-01-21 10:00:53 +0600

    Code arranged for clearly understanding

--- src/app/controllers/UserController.php
+++ src/app/controllers/UserController.php
@@ -30,47 +30,58 @@ class UserController extends \BaseController {
      * @return Response
      */
     public function store() {
+
         $validator = Validator::make(Input::all(), User::$rules);
         $data = array();
+
         if ($validator->passes()) {
+
             $checkDublicate = User::first(['email' => Input::get('email')]);
+            
             if ($checkDublicate instanceOf User) {
+                
                 $data = array(
-                    'response' => 'ERROR',
-                    'message' => 'You have already been registared.',
-                    'code' => 203,
+                    'response'  => 'ERROR',
+                    'message'   => 'You have already been registared.',
+                    'code'      => 304,
                 );
+
             } else {
-                $user = new User;
-                $value = Input::get('first_name').Input::get('email').Input::get('last_name');
-                $token = $this->createToken($value);
-                $user->first_name = Input::get('first_name');
-                $user->last_name = Input::get('last_name');
-                $user->email = Input::get('email');
-                $user->password = Hash::make(Input::get('password'));
-                $user->phone = Input::get('phone');
-                $user->city = Input::get('city');
-                $user->country = Input::get('country');
-                $user->status_token = $token;
-                $user->token_expire_date = Carbon::now()->addDay();
-                $user->status = 0;
+
+                $user   = new User;
+                $value  = Input::get('first_name').Input::get('email').Input::get('last_name');
+                $token  = $this->createToken($value);
+
+                $user->first_name           = Input::get('first_name');
+                $user->last_name            = Input::get('last_name');
+                $user->email                = Input::get('email');
+                $user->password             = Hash::make(Input::get('password'));
+                $user->phone                = Input::get('phone');
+                $user->city                 = Input::get('city');
+                $user->country              = Input::get('country');
+                $user->status_token         = $token;
+                $user->token_expire_date    = Carbon::now()->addDay();
+                $user->status               = 0;
 
                 $user->save(true);
 
                 $id = $user->_id;
+
                 $data = array(
-                    'response' => 'OK',
-                    'message' => 'successfully registered',
+                    'response'          => 'OK',
+                    'message'           => 'successfully registered',
                     'verification_link' => Request::server('HTTP_HOST') . '/api/v1/auth/' . $token . '/verify/' . Crypt::encrypt($id),
-                    'id' => $id,
-                    'code' => 200,
+                    'id'                => $id,
+                    'code'              => 200,
                 );
             }
+
         } else {
+
             // validation has failed, display error messages
             $data = array(
-                'message' => 'Validation failed not registered',
-                'code' => 202,
+                'message'   => 'Validation failed not registered',
+                'code'      => 400,
             );
         }
 
@@ -84,7 +95,9 @@ class UserController extends \BaseController {
      * @return Response
      */
     public function createToken($value) {
+        
         $token_value = $value.time();
+
         return $token = Hash::make($token_value);
     }
 
@@ -95,48 +108,60 @@ class UserController extends \BaseController {
      * @return Response
      */
     public function requestToken() {
+
         $checkExistance = User::first(['email' => Input::get('email')]);
+
         if ($checkExistance instanceOf User) {
-            $token_value = $checkExistance->first_name.$checkExistance->email.$checkExistance->last_name;
-            $token = $this->createToken($token_value);
-            $id = $checkExistance->_id;
+
+            $token_value    = $checkExistance->first_name.$checkExistance->email.$checkExistance->last_name;
+            $token          = $this->createToken($token_value);
+            $id             = $checkExistance->_id;
+
             if (Input::get('sector') == "email-verification") {
-                $checkExistance->status_token = $token;
-                $checkExistance->token_expire_date = Carbon::now()->addDay();
+
+                $checkExistance->status_token       = $token;
+                $checkExistance->token_expire_date  = Carbon::now()->addDay();
                 $checkExistance->save(true);
 
                 $data = array(
-                    'response' => 'OK',
-                    'message' => 'New token created and mailed to your email.',
+                    'response'          => 'OK',
+                    'message'           => 'New token created and mailed to your email.',
                     'verification_link' => Request::server('HTTP_HOST') . '/api/v1/auth/' . $token . '/verify/' . Crypt::encrypt($id),
-                    'id' => $id,
-                    'code' => 200,
+                    'id'                => $id,
+                    'code'              => 200,
                 );
+
             } else if (Input::get('sector') == "forgot-password") {
+
                 if ($checkExistance->status == 0) {
+                    
                     $data = array(
-                        'response' => 'ERROR',
-                        'message' => 'Your email is not verified yet. Please verify your email first.',
-                        'code' => 203,
+                        'response'  => 'ERROR',
+                        'message'   => 'Your email is not verified yet. Please verify your email first.',
+                        'code'      => 401,
                     );
+
                 } else {
-                    $checkExistance->status_token = $token;
-                    $checkExistance->token_expire_date = Carbon::now()->addDay();
+
+                    $checkExistance->status_token       = $token;
+                    $checkExistance->token_expire_date  = Carbon::now()->addDay();
                     $checkExistance->save(true);
 
                     $data = array(
-                        'response' => 'OK',
-                        'message' => 'New token created and mailed to your email.',
-                        'token' => $token,
-                        'id' => Crypt::encrypt($id),
-                        'code' => 200,
+                        'response'  => 'OK',
+                        'message'   => 'New token created and mailed to your email.',
+                        'token'     => $token,
+                        'id'        => Crypt::encrypt($id),
+                        'code'      => 200,
                     );
                 }
+
             }
         } else {
+
             $data = array(
-                'message' => 'Email address is not currect.',
-                'code' => 203,
+                'message'   => 'Email address is not currect.',
+                'code'      => 400,
             );
         }
 
@@ -150,6 +175,7 @@ class UserController extends \BaseController {
      * @return Response
      */
     public function show($id) {
+
         $data = array();
 
         $user = User::find($id);
@@ -157,15 +183,16 @@ class UserController extends \BaseController {
 
         if (isset($user->email)) {
 
-            $data = $user;
-            $data['code'] = 200;
-            $data['response'] = 'OK';
+            $data               = $user;
+            $data['code']       = 200;
+            $data['response']   = 'OK';
+
         } else {
 
             $data = array(
-                'response' => 'ERROR',
-                'message' => 'user not found',
-                'code' => 203,
+                'response'  => 'ERROR',
+                'message'   => 'user not found',
+                'code'      => 400,
             );
         }
 
@@ -179,6 +206,7 @@ class UserController extends \BaseController {
      * @return Response
      */
     public function edit($id) {
+
         $data = array();
 
         $user = User::find($id);
@@ -186,15 +214,16 @@ class UserController extends \BaseController {
 
         if (isset($user->email)) {
 
-            $data = $user;
-            $data['code'] = 200;
-            $data['response'] = 'OK';
+            $data               = $user;
+            $data['code']       = 200;
+            $data['response']   = 'OK';
+
         } else {
 
             $data = array(
-                'response' => 'ERROR',
-                'message' => 'user not found',
-                'code' => 204,
+                'response'  => 'ERROR',
+                'message'   => 'user not found',
+                'code'      => 400,
             );
         }
 
@@ -208,33 +237,35 @@ class UserController extends \BaseController {
      * @return Response
      */
     public function update($id) {
+
         $data = array();
 
         $user = User::first($id);
 
         if (isset($user->email)) {
 
-            $user->first_name = Input::get('first_name');
-            $user->last_name = Input::get('last_name');
-            $user->email = Input::get('email');
-            $user->password = Hash::make(Input::get('password'));
-            $user->phone = Input::get('phone');
-            $user->city = Input::get('city');
-            $user->country = Input::get('country');
+            $user->first_name   = Input::get('first_name');
+            $user->last_name    = Input::get('last_name');
+            $user->email        = Input::get('email');
+            $user->password     = Hash::make(Input::get('password'));
+            $user->phone        = Input::get('phone');
+            $user->city         = Input::get('city');
+            $user->country      = Input::get('country');
 
             $user->save(true);
 
             $data = array(
-                'response' => 'OK',
-                'message' => 'user successfully updated.',
-                'code' => 200,
+                'response'  => 'OK',
+                'message'   => 'user successfully updated.',
+                'code'      => 200,
             );
+
         } else {
 
             $data = array(
-                'response' => 'ERROR',
-                'message' => 'user not found',
-                'code' => 205,
+                'response'  => 'ERROR',
+                'message'   => 'user not found',
+                'code'      => 400,
             );
         }
 
@@ -248,6 +279,7 @@ class UserController extends \BaseController {
      * @return Response
      */
     public function destroy($id) {
+
         $user = User::first($id);
 
         if (isset($user)) {
@@ -255,16 +287,17 @@ class UserController extends \BaseController {
             $user->delete();
 
             $data = array(
-                'response' => 'OK',
-                'message' => 'user successfully deleted.',
-                'code' => 200,
+                'response'  => 'OK',
+                'message'   => 'user successfully deleted.',
+                'code'      => 200,
             );
+
         } else {
 
             $data = array(
-                'response' => 'ERROR',
-                'message' => 'user not found',
-                'code' => 206,
+                'response'  => 'ERROR',
+                'message'   => 'user not found',
+                'code'      => 400,
             );
         }
 
@@ -274,18 +307,22 @@ class UserController extends \BaseController {
     public function login() {
 
         $data = array();
-//        if (Auth::attempt(array('email' => Input::get('email'), 'password' => Input::get('password')))) {
+        //if (Auth::attempt(array('email' => Input::get('email'), 'password' => Input::get('password')))) {
+
         if (1) {
+
             $data = array(
-                'response' => 'OK',
-                'message' => 'You are now logged in!',
-                'code' => 200,
+                'response'  => 'OK',
+                'message'   => 'You are now logged in!',
+                'code'      => 200,
             );
+
         } else {
+            
             $data = array(
-                'response' => 'ERROR',
-                'message' => 'Your username/password combination was incorrect',
-                'code' => 201,
+                'response'  => 'ERROR',
+                'message'   => 'Your username/password combination was incorrect',
+                'code'      => 400,
             );
         }
         return $data;
@@ -298,34 +335,40 @@ class UserController extends \BaseController {
      * @return Response
      */
     public function verifyUser($token, $id) {
-        $data = array();
-        $id = Crypt::decrypt($id);
+
+        $data           = array();
+        $id             = Crypt::decrypt($id);
         $checkExistance = User::first(['_id' => $id, 'status_token' => $token]);
+
         if ($checkExistance instanceOf User) {
 
             if (Carbon::now() <= $checkExistance->token_expire_date["date"]) {
+
                 if ($checkExistance->status == 0) {
 
                     $checkExistance->status = 1;
                     $checkExistance->save(true);
 
                     $data = array(
-                        'response' => 'OK',
-                        'message' => 'Email successfully verified',
-                        'code' => 203,
+                        'response'  => 'OK',
+                        'message'   => 'Email successfully verified',
+                        'code'      => 200,
                     );
+
                 } else {
                     $data = array(
-                        'response' => 'ERROR',
-                        'message' => 'Your email is already verified.',
-                        'code' => 203,
+                        'response'  => 'ERROR',
+                        'message'   => 'Your email is already verified.',
+                        'code'      => 203,
                     );
                 }
+
             } else {
+
                 $data = array(
-                    'response' => 'ERROR',
-                    'message' => 'Email validation token expired. You need to request for a new one',
-                    'code' => 203,
+                    'response'  => 'ERROR',
+                    'message'   => 'Email validation token expired. You need to request for a new one',
+                    'code'      => 304,
                 );
             }
         }
@@ -340,35 +383,43 @@ class UserController extends \BaseController {
      * @return Response
      */
     public function forgotPassword() {
-        $data = array();
-        $id = Crypt::decrypt(Input::get('id'));
-        $token = Input::get('token');
+
+        $data   = array();
+        $id     = Crypt::decrypt(Input::get('id'));
+        $token  = Input::get('token');
         
         $checkExistance = User::first(['_id' => $id, 'status_token' => $token]);
 
         if ($checkExistance instanceOf User) {
+
             if (Carbon::now() <= $checkExistance->token_expire_date["date"]) {
+
                 $checkExistance->password = Hash::make(Input::get('password'));
                 $checkExistance->save(true);
 
                 $data = array(
-                    'response' => 'OK',
-                    'message' => 'Password changed successfully.',
-                    'code' => 200,
+                    'response'  => 'OK',
+                    'message'   => 'Password changed successfully.',
+                    'code'      => 200,
                 );
+
             } else {
+
                 $data = array(
-                    'response' => 'ERROR',
-                    'message' => 'Forgot password token expired. You need to request for a new one',
-                    'code' => 203,
+                    'response'  => 'ERROR',
+                    'message'   => 'Forgot password token expired. You need to request for a new one',
+                    'code'      => 403,
                 );
             }
+
         } else {
+            
             $data = array(
-                'response' => 'ERROR',
-                'message' => 'Not a valid user.',
-                'code' => 203,
+                'response'  => 'ERROR',
+                'message'   => 'Not a valid user.',
+                'code'      => 203,
             );
+
         }
 
         return $data;
--- src/app/routes.php
+++ src/app/routes.php
@@ -19,13 +19,20 @@ Route::get('/', function() {
 // Route::get('user/create', ['as' => 'login', 'uses' => 'LoginController@login']);
 
 Route::group(array('prefix' => 'api/v1'), function() {
+
   Route::resource('user', 'UserController');
 
   // custom method for user login and registration
   Route::post('user/login', 'UserController@login');
+
   Route::get('auth/{token}/verify/{id}', 'UserController@verifyUser');
+
   Route::post('auth/request-token', 'UserController@requestToken');
+
   Route::post('auth/change-password', 'UserController@forgotPassword');
+
   Route::post('city/get-city', 'CountryController@getCity');
+  
   Route::get('country/get-country', 'CountryController@getCountry');
+
 });
