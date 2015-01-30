cc79a945fe3ede92388fcd6559762384dd35f9ab
sakibdip <nazmus.sakib.sust@gmail.com>
2015-01-31 01:03:54 +0600

    Add functionality for Brands

--- src/app/controllers/ProductController.php
+++ src/app/controllers/ProductController.php
@@ -206,5 +206,17 @@ class ProductController extends BaseController {
     public function getCategory() {
         return Category::all();
     }
+    
+    public function getBrands() {
+        return Brand::all();
+    }
+    
+    public function saveBrands() {
+        $brands = new Brand;
+        $brands->name = Input::get('name');
+        $brands->save();
+        
+        return "ok";
+    }
 
 }
