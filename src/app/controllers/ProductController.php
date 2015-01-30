e96e3ec8575f705b795df1a6039e88a8e5484fcc
imran cluster <imrancluster@gmail.com>
2015-01-30 23:57:06 +0600

    updated product update, delete

--- src/app/controllers/ProductController.php
+++ src/app/controllers/ProductController.php
@@ -12,16 +12,16 @@ class ProductController extends BaseController {
         return Product::all();
     }
 
-  /**
-   * Show the form for creating a new resource.
-   *
-   * @return Response
-   */
-  public function create() {
+    /**
+    * Show the form for creating a new resource.
+    *
+    * @return Response
+    */
+    public function create() {
 
 
-    return array('message' => 'Form show.');
-  }
+        return array('message' => 'Form show.');
+    }
 
     /**
     * Store a newly created resource in storage.
@@ -56,7 +56,7 @@ class ProductController extends BaseController {
         $product = (object) $product;
 
         return $data;
-  }
+    }
 
     /**
     * Display the specified resource.
@@ -70,7 +70,7 @@ class ProductController extends BaseController {
 
         $product = Product::find($id);
 
-        if ($product->price != "") {
+        if ( isset($product->title) ) {
 
             $data               = $product;
             $data['response']   = "OK";
@@ -148,41 +148,63 @@ class ProductController extends BaseController {
                 'code'      => 400,
             );
         }
-        
+
         return $data;
     }
 
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function destroy($id) {
-    return $data;
-  }
+    /**
+    * Remove the specified resource from storage.
+    *
+    * @param  int  $id
+    * @return Response
+    */
+    public function destroy($id) {
 
-  public function getProductBySubCategory($category_id, $sub_category_id) {
-    $product = Product::where(['category' => $category_id, 'sub_category' => $sub_category_id]);
-    return $product;
-  }
-  public function getProductBySCategory($category_id) {
-    $product = Product::where(['category' => $category_id]);
-    return $product;
-  }
-  
-  public function upload() {
+        $product = Product::first($id);
 
-    if (Input::file('file')->move(__DIR__ . '/../storage/catalog/product/', Input::file('file')->getClientOriginalName())) {
-      return true;
+        if (isset($product)) {
+
+            $product->delete();
+
+            $data = array(
+                'response'  => 'OK',
+                'message'   => 'product successfully deleted.',
+                'code'      => 200,
+            );
+
+        } else {
+
+            $data = array(
+                'response'  => 'ERROR',
+                'message'   => 'user not found',
+                'code'      => 400,
+            );
+        }
+
+        return $data;
     }
-    else {
-      return false;
-    }
-  }
 
-  public function getCategory() {
-    return Category::all();
-  }
+    public function getProductBySubCategory($category_id, $sub_category_id) {
+        $product = Product::where(['category' => $category_id, 'sub_category' => $sub_category_id]);
+        return $product;
+    }
+    public function getProductBySCategory($category_id) {
+        $product = Product::where(['category' => $category_id]);
+        return $product;
+    }
+
+    public function upload() {
+
+        if (Input::file('file')->move(__DIR__ . '/../storage/catalog/product/', Input::file('file')->getClientOriginalName())) {
+          return true;
+        }
+        else {
+          return false;
+        }
+    }
+
+    public function getCategory() {
+        return Category::all();
+    }
 
 }
