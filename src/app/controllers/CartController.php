0026cd7f8e89ad1adc944c32d498365e8fb2f7fb
sakibdip <nazmus.sakib.sust@gmail.com>
2015-01-31 11:11:01 +0600

    Some new functionality added

--- src/app/controllers/CartController.php
+++ src/app/controllers/CartController.php
@@ -1,119 +1,178 @@
 <?php
+
 class CartController extends \BaseController {
-    
-    public function index(){ 
+
+    public function index() {
         //return Cart::instance('shopping')->destroy();
         $data = array();
         $product_list = Cart::instance('shopping')->content();
         //return $product_list;
         $count = Cart::instance('shopping')->count(false);
         $total_price = Cart::instance('shopping')->total();
-    
+
         $data = array(
-                    'response'          => 'OK',
-                    'message'           => 'Cart list returned successfully.',
-                    'products'          => $product_list,
-                    'total_price'       => $total_price,
-                    'count'             => $count,
-                    'code'              => 200,
-                );
+            'response' => 'OK',
+            'message' => 'Cart list returned successfully.',
+            'products' => $product_list,
+            'total_price' => $total_price,
+            'count' => $count,
+            'code' => 200,
+        );
         return $data;
     }
-    
-    public function store(){  
+
+    public function store() {
         $data = array();
         $product_id = Input::get('_id');
-        
+
         //return $product_id;
-        
+
         $p_value = Product::first(['_id' => $product_id]);
         Cart::instance('shopping')->add(array('id' => $p_value->_id, 'name' => $p_value->title, 'qty' => 1, 'price' => $p_value->price));
 //        Cart::instance('wishlist')->add(array('id' => '294ad', 'name' => 'Product 1', 'qty' => 2, 'price' => 9.99, 'options' => array('size' => 'large')));
 
+        $cartContentString = serialize(Cart::instance('shopping')->content());
+
+        $current_stored = CartProduct::first(['user_id' => $user_id, 'sector' => 'cart']);
+        if ($current_stored instanceof CartProduct) {
+            $current_stored->bulk_data = $cartContentString;
+            $current_stored->sector = 'cart';
+            $current_stored->save();
+        } else {
+            $cart_product = new CartProduct;
+            $cart_product->user_id = Input::get('user_id');
+            $cart_product->bulk_data = $cartContentString;
+            $current_stored->sector = 'cart';
+            $cart_product->save();
+        }
+
         $data = array(
-                    'response'          => 'OK',
-                    'message'           => 'Product successfully added to cart.',
-                    'code'              => 200,
-                );
+            'response' => 'OK',
+            'message' => 'Product successfully added to cart.',
+            'code' => 200,
+        );
         return $data;
     }
 
-    public function destroy($id) {  
+    public function destroy($id) {
         $data = array();
         $row_id = $id;
         Cart::instance('shopping')->remove($row_id);
-        
+
         $data = array(
-                    'response'          => 'OK',
-                    'message'           => 'Product successfully removed from cart.',
-                    'code'              => 200,
-                );
+            'response' => 'OK',
+            'message' => 'Product successfully removed from cart.',
+            'code' => 200,
+        );
         return $data;
     }
-    
-    public function update($id){  
+
+    public function update($id) {
         $data = array();
         $row_id = $id;
         Cart::instance('shopping')->update($row_id, array('qty' => Input::get('qty')));
-        
+
         $data = array(
-                    'response'          => 'OK',
-                    'message'           => 'Cart successfully updated.',
-                    'code'              => 200,
-                );
+            'response' => 'OK',
+            'message' => 'Cart successfully updated.',
+            'code' => 200,
+        );
         return $data;
     }
-    
-    
+
     /*
      * Wishlist sector started from here....
      */
-    
-    public function addToWishlist(){  
+
+    public function addToWishlist() {
         $data = array();
         $product_id = Input::get('_id');
-        
+
         //return $product_id;
-        
+
         $p_value = Product::first($product_id);
         Cart::instance('wishlist')->add(array('id' => $p_value->_id, 'name' => $p_value->title, 'qty' => 1, 'price' => $p_value->price));
 //        Cart::instance('wishlist')->add(array('id' => '294ad', 'name' => 'Product 1', 'qty' => 2, 'price' => 9.99, 'options' => array('size' => 'large')));
-    
+        $cartContentString = serialize(Cart::instance('wishlist')->content());
+
+        $current_stored = CartProduct::first(['user_id' => $user_id, 'sector' => 'wishlist']);
+        if ($current_stored instanceof CartProduct) {
+            $current_stored->bulk_data = $cartContentString;
+            $current_stored->sector = 'wishlist';
+            $current_stored->save();
+        } else {
+            $cart_product = new CartProduct;
+            $cart_product->user_id = Input::get('user_id');
+            $cart_product->bulk_data = $cartContentString;
+            $current_stored->sector = 'wishlist';
+            $cart_product->save();
+        }
         $data = array(
-                    'response'          => 'OK',
-                    'message'           => 'Product successfully added to your wishlist.',
-                    'code'              => 200,
-                );
+            'response' => 'OK',
+            'message' => 'Product successfully added to your wishlist.',
+            'code' => 200,
+        );
         return $data;
     }
-    
-    public function showWishlistProducts(){  
+
+    public function showWishlistProducts() {
         $data = array();
         $product_list = Cart::instance('wishlist')->content();
         $count = Cart::instance('wishlist')->count(false);
         $total_price = Cart::instance('wishlist')->total();
-    
+
         $data = array(
-                    'response'          => 'OK',
-                    'message'           => 'Wishlist returned successfully.',
-                    'products'          => $product_list,
-                    'total_price'       => $total_price,
-                    'count'             => $count,
-                    'code'              => 200,
-                );
+            'response' => 'OK',
+            'message' => 'Wishlist returned successfully.',
+            'products' => $product_list,
+            'total_price' => $total_price,
+            'count' => $count,
+            'code' => 200,
+        );
         return $data;
     }
-    
-    public function deleteWishlistProduct(){  
+
+    public function showCartForUser($id) {
+        //return Cart::instance('shopping')->destroy();
+        $data = array();
+        $current_stored = CartProduct::first(['user_id' => $user_id, 'sector' => 'cart']);
+        if ($current_stored instanceof CartProduct) {
+            $cartArray = unserialize($current_stored->bulk_data);
+            $data = $cartArray;
+            $data = array(
+                'response' => 'OK',
+                'message' => 'Cart retrive successfully.',
+                'code' => 200
+            );
+        } else {
+            $product_list = Cart::instance('shopping')->content();
+            //return $product_list;
+            $count = Cart::instance('shopping')->count(false);
+            $total_price = Cart::instance('shopping')->total();
+
+            $data = array(
+                'response' => 'OK',
+                'message' => 'Cart list returned successfully.',
+                'products' => $product_list,
+                'total_price' => $total_price,
+                'count' => $count,
+                'code' => 200,
+            );
+        }
+        return $data;
+    }
+
+    public function deleteWishlistProduct() {
         $data = array();
         $row_id = Input::get('rowid');
         Cart::instance('wishlist')->remove($row_id);
-        
+
         $data = array(
-                    'response'          => 'OK',
-                    'message'           => 'Product successfully removed from wishlist.',
-                    'code'              => 200,
-                );
+            'response' => 'OK',
+            'message' => 'Product successfully removed from wishlist.',
+            'code' => 200,
+        );
         return $data;
     }
+
 }
---
+++ src/app/models/CartProduct.php
@@ -0,0 +1,11 @@
+<?php
+
+class CartProduct extends MongoLid {
+
+    /**
+     * The database collection used by the model.
+     *
+     * @var string
+     */
+    protected $collection = 'Cartproducts';
+}
